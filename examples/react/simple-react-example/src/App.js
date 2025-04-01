7ed7c5d96e817d7c42268114547d8daca68d0a36
Pavle Rogan <pavle@divstechnology.com>
2025-04-01 14:09:07 +0200

    remove manual integration

--- examples/react/simple-react-example/src/App.js
+++ examples/react/simple-react-example/src/App.js
@@ -2,56 +2,16 @@ import { useEffect, useState } from "react";
 import { MoveoOne } from "moveo-one-analytics-js";
 import "./App.css";
 
-const moveoInstance = MoveoOne.getInstance("<YOUR_API_TOKEN>");
-
-moveoInstance.setLogging(true);
-moveoInstance.setFlushInterval(5000);
-
 function App() {
   const [inputText, setInputText] = useState("");
 
-  useEffect(() => {
-    moveoInstance.identify("<USER_ID>");
-    moveoInstance.start("main_screen", {
-      app_version: "1.0.0",
-      platform: "web",
-    });
-
-    // Paragraph tracking (tick)
-    moveoInstance.tick({
-      semanticGroup: "content_section",
-      id: "intro_paragraph",
-      type: "text",
-      action: "view",
-      value: "demo_description",
-      metadata: { content_type: "static_text" },
-    });
-  }, []);
+  useEffect(() => {}, []);
 
   const handlePress = (buttonName) => {
-    // Button tracking (track)
-    moveoInstance.track("main_screen", {
-      semanticGroup: "action_section",
-      id: `${buttonName.toLowerCase().replace(" ", "_")}_button`,
-      type: "button",
-      action: "click",
-      value: buttonName.toLowerCase(),
-      metadata: { action_type: "user_click" },
-    });
     console.log(`${buttonName} clicked!`);
   };
 
-  const handleInputBlur = () => {
-    // Input tracking (track)
-    moveoInstance.track("main_screen", {
-      semanticGroup: "form_section",
-      id: "main_input",
-      type: "input",
-      action: "edit",
-      value: "text_update",
-      metadata: { input_type: "free_text" },
-    });
-  };
+  const handleInputBlur = () => {};
 
   return (
     <div className="main-container">
