5f279e8b1fa28cece942a1619d00365138a6b853
MoveoOne Integration <integration@moveo.one>
2025-04-01 12:24:46 +0000

    MoveoOne automatic integration.

--- examples/react/simple-react-example/src/App.js
+++ examples/react/simple-react-example/src/App.js
@@ -2,16 +2,44 @@ import { useEffect, useState } from "react";
 import { MoveoOne } from "moveo-one-analytics-js";
 import "./App.css";
 
+const analytics = MoveoOne.getInstance('<YOUR_SDK_TOKEN>');
+
 function App() {
   const [inputText, setInputText] = useState("");
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    analytics.start('main_screen', {
+      version: '1.0.0',
+      environment: 'development'
+    });
+
+    return () => {
+      analytics.stop();
+    };
+  }, []);
 
   const handlePress = (buttonName) => {
     console.log(`${buttonName} clicked!`);
+    analytics.track('main_screen', {
+      semanticGroup: 'button_group',
+      id: buttonName.toLowerCase().replace(' ', '_'),
+      type: 'button',
+      action: 'click',
+      value: buttonName,
+      metadata: { location: 'button_group' }
+    });
   };
 
-  const handleInputBlur = () => {};
+  const handleInputBlur = () => {
+    analytics.track('main_screen', {
+      semanticGroup: 'input_field',
+      id: 'input_field',
+      type: 'input',
+      action: 'blur',
+      value: inputText,
+      metadata: { placeholder: 'Type something...' }
+    });
+  };
 
   return (
     <div className="main-container">
