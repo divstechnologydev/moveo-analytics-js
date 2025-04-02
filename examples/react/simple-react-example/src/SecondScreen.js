89f343658b47021c221291ac7559a4ac6c26597a
Pavle Rogan <pavle@divstechnology.com>
2025-04-02 09:49:59 +0200

    Button on second screen

--- examples/react/simple-react-example/src/SecondScreen.js
+++ examples/react/simple-react-example/src/SecondScreen.js
@@ -6,10 +6,22 @@ function SecondScreen() {
     console.log("Second screen loaded");
   }, []);
 
+  const handlePress = (buttonName) => {
+    console.log(`${buttonName} clicked!`);
+  };
+
   return (
     <div className="main-container">
       <h1 className="title">Second Screen</h1>
       <p className="paragraph">Welcome to the second screen!</p>
+      <div className="button-group">
+        <button
+          className="button primary"
+          onClick={() => handlePress("Button")}
+        >
+          Button
+        </button>
+      </div>
     </div>
   );
 }
